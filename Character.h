a77a034f4ef9abdc0f8ac7f0bab25d554349fcec
Sabanrab <austinlittley@gmail.com>
2016-02-27 11:54:28 -0500

    fixes include issue

--- Character.h
+++ Character.h
@@ -2,16 +2,29 @@
 #define CHARACTER_H
 
 #include <string>
-using std::string
+using std::string;
 #include <deque>
 using std::deque;
 
 #include "Location.h"
+
+class Location;
 
 class Character{
 public:
 	string name;
 	Location* charLocation;
 };
+
+class Player : public Character {
+public:
+	
+};
+
+class NPC :public Character {
+public:
+
+};
+
 
 #endif
