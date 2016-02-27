2b495d1ecf28b31cd19d981b6de5a66d8f6bb12b
Sabanrab <austinlittley@gmail.com>
2016-02-26 20:57:35 -0500

    Update player.h

--- player.h
+++ player.h
@@ -1,6 +1,19 @@
 #ifndef __PLAYER_H_INCLUDED__
 #define __PLAYER_H_INCLUDED__
 
+#include <sting>
+using std::string;
 
+class Player{
+  string name;
+  string gender;
+  string hairColor;
+  string body;
+  string skinColor;
+  long relationship;
+  deque<Items> inventory;
+  deque<Quests> questDeque;
+  Location currentLocation;
+}
 
 #endif
