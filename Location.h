92a577e9675988e9054139c1681dadb12ab37d62
Sabanrab <austinlittley@gmail.com>
2016-02-26 21:46:00 -0500

    Update Location.h

--- Location.h
+++ Location.h
@@ -1,8 +1,29 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
+#include<string>
+using std::string;
+#include<deque>
+using std::deque;
+
+#include "NPC.h"
+#include "Item.h"
+#include "Quest.h"
+#include "Skill.h"
+#include "Location.h"
+#include "Attributes.h"
+
 class Location{
-    
+    string name;
+    string status; //in or out
+    location* north;
+    location* south;
+    location* east;
+    location* west;
+    location* northEast;
+    location* northWest;
+    location* southEast;
+    location* southWest;
 };
 
 
