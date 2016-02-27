9dbe99d5859f18c9058f802e853421ad9862cef4
Sabanrab <austinlittley@gmail.com>
2016-02-26 23:45:31 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,6 +5,8 @@
 using std::string;
 #include <deque>
 using std::deque;
+#include <iostream>
+using std::cout; using std::endl;
 
 #include "NPC.h"
 #include "Item.h"
@@ -28,13 +30,24 @@ class Player{
   
   //dest is requested destination
   void move (string dest,Location* start){
-    bool valid = 0; //if the destination location is ok
-    for(auto elem : location->destinations){
-      if(elem.first->name==dest && elem.second ==1){
-        playerLocation
+    for(auto elem : location->destinations){ //go through possible destinations
+      if(elem.first->name==dest && elem.second ==1){ //if destination exists and path is open
+        currentLocation = elem.first; //perform the move
+        cout << "***" << currentLocation.name << "***" << endl;
+        if(currentLocation.status == 0){ //if the location has not been visited before
+          cout << currentLocation.description << endl; //print the description
+          currentLocation.status = 1; //record that location has been visited
+        }
+      }
+      else{
+        cout << "Invalid move";
       }
     }
   }
+  
+//  void pickUp (string target, Location* current){
+//    for(auto elem : location->)
+//  }
 };
 
 #endif
