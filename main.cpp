1dd118cc7c2863a4d069763c54be5d67fe93b661
Sabanrab <austinlittley@gmail.com>
2016-02-27 11:12:16 -0500

    more testing

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 using std::cin; using std::cout; using std::endl;
 #include <deque>
 using std::deque;
+#include <utility>
+using std::make_pair; using std::pair;
 #include "Location.h"
 
 int main(){
+	Location tester;
+	Location linker;
+	tester.name = "your mother";
+	linker.name = "dan's mother";
+	tester.destinations.push_back(make_pair(&linker,0));
+	cout << tester.destinations[0].first->name << endl;
 	return 0;
 }
