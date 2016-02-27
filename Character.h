58c8907306faf357e38ec169e5c28fa9ea47be17
Sabanrab <austinlittley@gmail.com>
2016-02-27 13:02:15 -0500

    adds new data members without implementation

--- Character.h
+++ Character.h
@@ -14,6 +14,10 @@ class Item;
 class Character{
 public:
 	string name;
+	string gender;
+	string hairColor;
+	string skinColor;
+	string bodyType;
 	Location* charLocation;
 	deque<Item> inventory;
 };
@@ -22,7 +26,6 @@ public:
 
 class Player : public Character {
 public:
-
     //these are the types of attributes each character has but we don't know
     //what they do yet
     long intelligence = 0;  // 
@@ -34,15 +37,11 @@ public:
     long reliability = 0;   //
     long intimidation = 0;  // player can choose to go the intimidation route
     long reputation = 0;    //
-
-
-
-	
 };
 
 class NPC :public Character {
 public:
-
+	long opinion; //what the npc thinks about the player
 };
 
 
