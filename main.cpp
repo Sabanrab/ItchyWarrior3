42c2aa061c8377af67c36eb355cee5e16618a90e
Sabanrab <austinlittley@gmail.com>
2016-02-27 15:26:27 -0500

    sdf

--- main.cpp
+++ main.cpp
@@ -9,11 +9,24 @@ using std::make_pair; using std::pair;
 
 int main(){
 	NPC testerNPC;
-	testerNPC.name = "philip";
+	Player testerPlayer;
 	Location testerPlace;
+	testerPlace._name = "bobty";
 	Item testerItem;
-	testerItem.name = "sword";
-	testerPlace.objects.push_back(testerItem);
-	cout << testerPlace.objects[0].name << endl;
+	Item testerItem9;
+	Speech testerSpeech;
+	deque<Item> stuff = {};
+	Item thing;
+	thing._name = "YES IT IS";
+	stuff.push_back(thing);
+	Character Gry = Character("gary", "f","bule","prapel","fat as fuck",&testerPlace,stuff);
+
+//	cout << Gry._name << endl << Gry._gender << endl << Gry._hairColor << endl << Gry._skinColor\
+//<< endl << Gry._bodyType << endl;
+//	cout << Gry._charLocation->_name << endl;
+//	cout << Gry._inventory[0]->_name << endl;
+	
+
+
 	return 0;
 }
