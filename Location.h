53911049349542b506a9041f2f65cb5a1dace10a
Sabanrab <austinlittley@gmail.com>
2016-02-26 23:54:52 -0500

    Update Location.h

--- Location.h
+++ Location.h
@@ -6,23 +6,23 @@ using std::string;
 #include<deque>
 using std::deque;
 
-#include "NPC.h"
-#include "Item.h"
-#include "Quest.h"
-#include "Skill.h"
-#include "Location.h"
-#include "Attributes.h"
+//#include "NPC.h"
+//#include "Item.h"
+//#include "Quest.h"
+//#include "Skill.h"
+//#include "Location.h"
+//#include "Attributes.h"
 
 class Location{
     string name;
     string description;
     string status; //in or out
-    bool visited = 0; //if player has been here already or not
-    deque<pair<location*,bool>> destinations; //bool true if path open
-    deque<NPC> NPCS;
-    deque<Object> objects;
-    deque<Event> events;
-    deque<Item> items;
+    //bool visited = 0; //if player has been here already or not
+    //deque<pair<location*,bool>> destinations; //bool true if path open
+    //deque<NPC> NPCS;
+    //deque<Object> objects;
+    //deque<Event> events;
+    //deque<Item> items;
 };
 
 #endif /* LOCATION_H */
