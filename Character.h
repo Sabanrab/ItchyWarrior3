30941485d204f657bfaab8ee0c42367bd2f0e7a4
Daniel Shumaker <shumak37@adriatic.cse.msu.edu>
2016-02-27 12:56:52 -0500

    Player Attributes

--- Character.h
+++ Character.h
@@ -23,7 +23,17 @@ public:
 class Player : public Character {
 public:
 
-
+    //these are the types of attributes each character has but we don't know
+    //what they do yet
+    long intelligence = 0;  // 
+    long strength = 0;      //  
+    long relationship = 0;  // 
+    long charisma = 0;      // 
+    long positivity = 0;    //
+    long manipulation = 0;  // 
+    long reliability = 0;   //
+    long intimidation = 0;  // player can choose to go the intimidation route
+    long reputation = 0;    //
 
 
 
