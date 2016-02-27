cd9ce655e63b1e8a069bf8253c5f56e0d775f132
deshumake <shumak37@msu.edu>
2016-02-26 20:59:22 -0500

    Create Attributes.h

---
+++ Attributes.h
@@ -0,0 +1,25 @@
+#ifndef ATTRIBUTES_H
+#define ATTRIBUTES_H
+#include <deque>
+
+using std::deque;
+
+
+//Attributes help determine 
+class Attributes{
+public:
+    //these are the types of attributes each character has but we don't know what they do yet
+    long intelligence = 0;  // 
+    long strength = 0;      //  
+    long relationship = 0;  // 
+    long charisma = 0;      // 
+    long positivity = 0;    //
+    long manipulation = 0;  // 
+    long reliability = 0;   //
+    long intimidation = 0;  // player can choose to go the intimidation route
+    long reputation = 0;    //
+    
+};
+
+
+#endif /* ATTRIBUTES_H */
