1cd22c81001570d73d33eed278128dff061ece61
Sabanrab <austinlittley@gmail.com>
2016-02-26 22:13:42 -0500

    Update Object.h

--- Object.h
+++ Object.h
@@ -1,8 +1,15 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <string>
+using std::string
+
+#include "Location.h"
+
 class Object{
-    
+    string name;
+    string description; //to be displayed when the user examines
+    Location* objectLocation;
 };
 
 
